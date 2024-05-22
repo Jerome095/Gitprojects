f9761c3a070aefbe000b777431e84a1bedeb7651
Jerome095 <jerome22095@gmail.com>
2024-05-22 13:41:38 +0530

    vishal done his work

--- src/org/sample/GitSample.java
+++ src/org/sample/GitSample.java
@@ -3,9 +3,11 @@ package org.sample;
 public class GitSample {
 	
 	public static void main(String[] args) {
-		
 		String s = "Git Project";
 		System.out.println(s);
+		
+		float b = 46.94f;
+		System.out.println(b);
 	}
 
 }
