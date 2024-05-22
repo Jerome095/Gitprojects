e83580eb156b702e899734a20d892009761c1c4f
Jerome095 <jerome22095@gmail.com>
2024-05-22 14:03:05 +0530

    updated code

--- src/org/sample/GitSample.java
+++ src/org/sample/GitSample.java
@@ -6,13 +6,14 @@ public class GitSample {
 		String s = "Git Project";
 		System.out.println(s);
 		
-<<<<<<< HEAD
-		float b = 46.94f;
-		System.out.println(b);
-=======
 		int a = 70;
 		System.out.println(a);
->>>>>>> 83bcf5121f112907ffd843fc91979df07853fbfe
+		
+
+		float b = 46.94f;
+		System.out.println(b);
+
+
 	}
 
 }
